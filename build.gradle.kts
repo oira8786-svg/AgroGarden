a601843e275b04b4478a6e3baa1c6e4897664d8d
oira8786-svg <oira8786@gmail.com>
2026-08-13 01:50:36 +0300

    Migrate plugin declarations to plugins block

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,5 @@
-id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
+plugins {
+    id("com.android.application") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
+}
